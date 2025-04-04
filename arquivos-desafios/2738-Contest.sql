7af5152876a86b832fd48f9c0e74b3a1a32a304b
Alex Benjamim de Oliveira Martins <155112179+alexoliveiramartins@users.noreply.github.com>
2025-04-04 11:37:36 -0300

    Update 2738-Contest.sql

--- arquivos-desafios/2738-Contest.sql
+++ arquivos-desafios/2738-Contest.sql
@@ -4,6 +4,7 @@
  * Escriba su solución aquí
  */
 
-SELECT c.name, (s.math*2 + s.specific*3 + s.project_plan*5)/10 AS avg FROM candidate as c
+SELECT c.name, CAST((s.math*2 + s.specific*3 + s.project_plan*5)/10 AS DECIMAL(10,2)) AS avg 
+FROM candidate as c
 JOIN score as s ON s.candidate_id = c.id
-
+ORDER BY avg DESC;
